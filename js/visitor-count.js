b602f805b193c8933a0341699bc20387c3218b88
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2020-05-12 00:16:29 -0400

    feat: add API calls to update DB with visitor counts

--- js/visitor-count.js
+++ js/visitor-count.js
@@ -1,40 +1,45 @@
-const api = 'https://bpkuubwj5d.execute-api.us-east-1.amazonaws.com/Prod'
 
-const postRequest = async (url, data) => {
-    url = `${api}/${url}`;
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    return response.json();
-};
-
-const getRequest = async (url) => {
-    url = `${api}/${url}`;
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin': api
-        }
-    });
-    return response.json();
-};
 
 const main = () => {
+    const api = 'https://bpkuubwj5d.execute-api.us-east-1.amazonaws.com/Prod/page';
+
+    const putRequest = async (url, data) => {
+        url = `${api}/${url}`;
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.status === 200) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    const getRequest = async (url) => {
+        url = `${api}/${url}`;
+        console.log(`URL: ${url}`);
+        const response = await fetch(url, {
+            method: 'GET'
+        });
+        return response.json();
+    };
     window.addEventListener('load', async () => {
         if (window.location.pathname === '/') {
-            console.log('hit the root');
             // Call API to add one visitor to root page count
             let currentCount = await getRequest('root');
             console.log(`You are vistor #${currentCount}!`);
             currentCount++;
-            await postRequest('root', {ViewCount: currentCount});
+            await putRequest('root', { ViewCount: currentCount });
         } else if (window.location.pathname === '/web-resume.html') {
-            console.log('hit the resume');
             // Call API to add one visitor to resume page count
+            let currentCount = await getRequest('resume');
+            console.log(`You are vistor #${currentCount}!`);
+            currentCount++;
+            await putRequest('resume', { ViewCount: currentCount});
         }
     });
 }
